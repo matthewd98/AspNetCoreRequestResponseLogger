5e671a6fb56dbe05089148cdf01b70cbfc8a019e
Matthew D'Addario <matt.daddario@hotmail.com>
2020-11-29 10:10:29 -0500

    Check in code

---
+++ CustomMiddlewareExtensions.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace RequestResponseLoggerMiddleware
+{
+    public static class CustomMiddlewareExtensions
+    {
+        public static void UseRequestResponseLogging(this IApplicationBuilder app) => app.UseMiddleware<RequestResponseLoggerMiddleware>();
+    }
+}
---
+++ RequestResponseLoggingMiddleware.cs
@@ -0,0 +1,74 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace RequestResponseLoggerMiddleware
+{
+    public class RequestResponseLoggerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly bool _isRequestResponseLoggingEnabled;
+
+        public RequestResponseLoggerMiddleware(RequestDelegate next, IConfiguration config)
+        {
+            _next = next;
+            _isRequestResponseLoggingEnabled = config.GetValue<bool>("EnableRequestResponseLogging", false);
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            if (_isRequestResponseLoggingEnabled)
+            {
+                Console.WriteLine($"HTTP request information:\n" +
+                    $"\tMethod: {httpContext.Request.Method}\n" +
+                    $"\tPath: {httpContext.Request.Path}\n" +
+                    $"\tQueryString: {httpContext.Request.QueryString}\n" +
+                    $"\tHeaders: {FormatHeaders(httpContext.Request.Headers)}\n" +
+                    $"\tSchema: {httpContext.Request.Scheme}\n" +
+                    $"\tHost: {httpContext.Request.Host}\n" +
+                    $"\tBody: {await ReadBodyFromRequest(httpContext.Request)}");
+
+                // Temporarily replace the HttpResponseStream, which is a write-only stream, with a MemoryStream to capture it's value in-flight.
+                var originalResponseBody = httpContext.Response.Body;
+                using var newResponseBody = new MemoryStream();
+                httpContext.Response.Body = newResponseBody;
+
+                await _next(httpContext);
+
+                newResponseBody.Seek(0, SeekOrigin.Begin);
+                var responseBodyText = await new StreamReader(httpContext.Response.Body).ReadToEndAsync();
+
+                Console.WriteLine($"HTTP request information:\n" +
+                    $"\tStatusCode: {httpContext.Response.StatusCode}\n" +
+                    $"\tContentType: {httpContext.Response.ContentType}\n" +
+                    $"\tHeaders: {FormatHeaders(httpContext.Response.Headers)}\n" +
+                    $"\tBody: {responseBodyText}");
+
+                newResponseBody.Seek(0, SeekOrigin.Begin);
+                await newResponseBody.CopyToAsync(originalResponseBody);
+            }
+            else
+            {
+                await _next(httpContext);
+            }
+        }
+
+        private static string FormatHeaders(IHeaderDictionary headers) => string.Join(", ", headers.Select(kvp => $"{{{kvp.Key}: {string.Join(", ", kvp.Value)}}}"));
+
+        private static async Task<string> ReadBodyFromRequest(HttpRequest request)
+        {
+            // Ensure the request's body can be read multiple times (for the next middlewares in the pipeline).
+            request.EnableBuffering();
+
+            using var streamReader = new StreamReader(request.Body, leaveOpen: true);
+            var requestBody = await streamReader.ReadToEndAsync();
+
+            // Reset the request's body stream position for next middleware in the pipeline.
+            request.Body.Position = 0;
+            return requestBody;
+        }
+    }
+}
