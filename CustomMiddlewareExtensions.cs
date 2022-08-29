4b7a8eb91b709c343774ee4dd8b8426188798b46
removedporn <86824510+removedporn@users.noreply.github.com>
2022-08-29 15:40:51 +0800

    Delete CustomMiddlewareExtensions.cs

--- CustomMiddlewareExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Builder;
-
-namespace RequestResponseLoggerMiddleware
-{
-    public static class CustomMiddlewareExtensions
-    {
-        public static void UseRequestResponseLogging(this IApplicationBuilder app) => app.UseMiddleware<RequestResponseLoggerMiddleware>();
-    }
-}
